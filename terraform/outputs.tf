f923b66555103bcafe69e570b13f10b07da80c11
Josh <joshhaile7@gmail.com>
2024-03-14 13:18:28 -0700

    use templatefile for state machine definition

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -26,6 +26,54 @@ output "pobit_audit_path"{
   value = var.pobit_audit_path
 }
 
-output "browse_image_state_machine_definition"{
-  value = aws_sfn_state_machine.sfn_state_machine.definition
+output "get_dataset_configuration_arn" {
+  value = aws_lambda_function.get_dataset_configuration.arn
+}
+
+output "get_granule_umm_json_arn" {
+  value = aws_lambda_function.get_granule_umm_json.arn
+}
+
+output "get_collection_concept_id_arn" {
+  value = aws_lambda_function.get_collection_concept_id.arn
+}
+
+output "identify_image_file_arn" {
+  value = aws_lambda_function.identify_image_file.arn
+}
+
+output "generate_image_metadata_arn" {
+  value = aws_lambda_function.generate_image_metadata.arn
+}
+
+output "submit_harmony_job_arn" {
+  value = aws_lambda_function.submit_harmony_job.arn
+}
+
+output "submit_harmony_job_function_name" {
+  value = aws_lambda_function.submit_harmony_job.function_name
+}
+
+output "get_harmony_job_status_arn" {
+  value = aws_lambda_function.get_harmony_job_status.arn
+}
+
+output "copy_harmony_output_to_s3_arn"{
+  value = aws_lambda_function.copy_harmony_output_to_s3.arn
+}
+
+output "apply_opera_treatment_arn"{
+  value = aws_lambda_function.apply_opera_treatment.arn
+}
+
+output "pobit_build_image_sets_arn" {
+  value = aws_lambda_function.build_image_sets.arn
+}
+
+output "pobit_send_to_gitc_arn" {
+  value = aws_lambda_function.send_to_gitc.arn
+}
+
+output "pobit_save_cma_message_arn" {
+  value = aws_lambda_function.save_cma_message.arn
 }
