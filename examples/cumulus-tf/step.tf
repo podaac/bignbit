05a1826e25e5719c891d20d07e61de920246ff34
Josh <joshhaile7@gmail.com>
2024-03-15 13:09:17 -0700

    restore step.tf

--- examples/cumulus-tf/step.tf
+++ examples/cumulus-tf/step.tf
@@ -1,13 +1,8 @@
-# In a typical cumulus installation, this is how you would define the workflow:
+# This example is deployable without cumulus installed for the purpose of testing the module in isolation; so it does not use terraform-aws-cumulus-workflow.zip
 
-
-module "browse_image_workflow" {
-  source          = "https://github.com/nasa/cumulus/releases/download/v16.1.2/terraform-aws-cumulus-workflow.zip"
-  prefix          = var.prefix
-  name            = "BrowseImageWorkflow"
-  workflow_config = module.cumulus.workflow_config
-  system_bucket   = var.system_bucket
-  tags            = merge(local.tags, { application = "BrowseImageWorkflow" })
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "${local.ec2_resources_name}-BrowseImageWorkflow"
+  role_arn = aws_iam_role.step.arn
 
   definition = templatefile("../../terraform/state_machine_definition.json", {
       GetDatasetConfigurationLambda = module.bignbit_module.get_dataset_configuration_arn,
