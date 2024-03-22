c5b332e2f944229ceacd74d5a80cc7f8a37e4d7c
Frank Greguska <89428916+frankinspace@users.noreply.github.com>
2024-03-21 17:46:28 -0700

    cleanup

--- examples/cumulus-tf/browse_image_workflow.tf
+++ examples/cumulus-tf/browse_image_workflow.tf
@@ -11,3 +11,11 @@ module "browse_image_workflow" {
   state_machine_definition = module.bignbit_module.workflow_definition
 }
 */
+
+# This example is deployable without cumulus installed for the purpose of testing the module in isolation; so it does not use terraform-aws-cumulus-workflow.zip
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "${local.ec2_resources_name}-BrowseImageWorkflow"
+  role_arn = aws_iam_role.step.arn
+
+  definition = module.bignbit_module.workflow_definition
+}
--- examples/cumulus-tf/step.tf
+++
@@ -1,7 +0,0 @@
-# This example is deployable without cumulus installed for the purpose of testing the module in isolation; so it does not use terraform-aws-cumulus-workflow.zip
-resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name     = "${local.ec2_resources_name}-BrowseImageWorkflow"
-  role_arn = aws_iam_role.step.arn
-
-  definition = module.bignbit_module.workflow_definition
-}
