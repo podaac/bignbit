3b49837fe4fe3ce71e5e425b6394c426eca60379
Simon Liu <simon.liu@jpl.nasa.gov>
2025-09-11 12:26:18 -0700

    update aws provider version to be between 5.0 and 6.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.9.0"
+      version = ">= 5.100, < 6.0.0"
     }
     null = {
       source  = "hashicorp/null"
