2d58bce399efc29d61699d28d2a4481bd4e42f7b
Jacqueline Ryan <Jacqueline.Ryan@jpl.nasa.gov>
2026-03-30 11:08:05 -0700

    issue/160: Change default values of timeout vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -121,17 +121,17 @@ variable "harmony_job_status_interval_seconds" {
 variable "harmony_job_status_max_attempts" {
     type = number
     description = "Maximum number of attempts to check Harmony job status"
-    default = 15
+    default = 50
 }
 
 variable "harmony_job_status_backoff_rate" {
     type = number
     description = "Backoff rate for Harmony job status checks"
-    default = 1.0
+    default = 1.1
 }
 
 variable "harmony_job_status_max_delay_seconds" {
     type = number
     description = "Maximum delay in seconds for Harmony job status checks"
-    default = 20
+    default = 60
 }
