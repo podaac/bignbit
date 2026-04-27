dd6a6aa2282456082a566f0aa72400df5994aaf7
David Wood <david.v.wood@nasa.gov>
2026-04-27 10:36:55 -0400

    removing doule json.loads

--- bignbit/utils.py
+++ bignbit/utils.py
@@ -360,8 +360,8 @@ def get_harmony_client(environment_str: str) -> Client:
         )
 
         # Read the payload from Lambda response and parse JSON
-        response_payload = json.loads(response["Payload"].read())
-        token = json.loads(response_payload)['access-token']
+        # response_payload = json.loads(response["Payload"].read())
+        token = json.loads(response["Payload"].read())['access-token']
 
         # Instantiate Harmony client with retrieved token
         HARMONY_CLIENT = Client(
