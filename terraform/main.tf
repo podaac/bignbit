70ffef32b10c33dae1cdbc6f1cd4016a2ccc870e
Jacqueline Ryan <Jacqueline.Ryan@jpl.nasa.gov>
2026-06-03 10:01:43 -0700

    issues/173: changed default ecr repo name to allow multiple deployments

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ locals {
 
   account_id = data.aws_caller_identity.current.account_id
 
-  aws_resources_name = terraform.workspace == "default" ? "svc-${var.app_name}-${var.prefix}" : "svc-${var.app_name}-${var.prefix}-${terraform.workspace}"
+  aws_resources_name = terraform.workspace == "default" ? "${var.app_name}-${var.prefix}" : "${var.app_name}-${var.prefix}-${terraform.workspace}"
 
   default_tags = length(var.default_tags) == 0 ? {
     team : "TVA",
